c8f0099b72e823c102a8007522e4d7b8cdd1d289
vishalvarghese46 <vishalvarghese46@gmail.com>
2019-07-01 22:54:02 +0100

    Starting with chat client code

---
+++ chat/chatClient.py
@@ -0,0 +1,39 @@
+import socket
+import select
+import errno
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Username: ")
+
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to a given ip and port
+client_socket.connect((IP, PORT))
+
+# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+client_socket.setblocking(False)
+
+# Prepare username and header and send them
+# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+client_socket.send(username_header + username)
+
+while True:
+
+    # Wait for user to input a message
+    message = input(f'{my_username} > ')
+
+    # If message is not empty - send it
+    if message:
+
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
--- chat/chatServer.py
+++ chat/chatServer.py
@@ -3,7 +3,7 @@ import select
 
 HEADER_LENGTH = 10
 
-IP = "127.0.0.1"
+IP = "192.168.1.142"
 PORT = 1234
 
 # Create a socket
